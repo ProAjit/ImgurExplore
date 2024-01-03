863b05814b67f6caea0fd6b20bfb9bd36b9b3617
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2024-01-03 23:02:05 +0530

    Update AppConstants.swift

--- ImgurExplore/AppConstants.swift
+++ ImgurExplore/AppConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct AppConstants {
     
 //    static let searchURL = "https://api.imgur.com/3/gallery/search/top/week/0?q=all"
-    static let client_id = "Client-ID 57c4393ec7ec2b1"
+//    static let client_id = "Client-ID 57c4393ec7ec2b1"
 
     static let accountImages = "https://api.imgur.com/3/account/me"
     static let accessToken = "Bearer f4641795bbb009d1eadf5a95153e33a6c2299545"
